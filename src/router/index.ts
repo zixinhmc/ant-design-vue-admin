d18cc697f9a31dc5a2c524b5b1c75a760cbaabb6
hmc <hmc@zhiruyi.com>
2020-12-09 20:16:25 +0800

    删除 .idea文件夹 新增Login.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import Login from '/@/views/sys/login/Login.vue';
+
 const router = createRouter({
   history: createWebHashHistory(),
   strict: true,
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: Login,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/sys/login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <Checkbox />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { Checkbox } from 'ant-design-vue';
+
+  export default defineComponent({
+    name: 'Login',
+    components: {
+      Checkbox,
+    },
+    setup() {
+      alert(10);
+    },
+  });
+</script>
+
+<style scoped></style>
